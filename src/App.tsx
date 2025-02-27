089870bdcaa6f7898e632da94c82ba2e8f3326d2
kumanboy <husandavronov13@gmail.com>
2025-02-27 10:09:01 +0500

    useReducer end

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
-import React from "react";
-import { LanguageProvider, useLanguage } from "./context/LanguageContext";
-import LanguageSwitcher from "./components/LanguageSwitcher";
+import Quiz from "./components/Quiz.tsx";
 
-// Dictionary for translations
-const translations = {
-    en: {
-        greeting: "Welcome to our website!",
-        description: "This is a simple language switcher example using useContext.",
-    },
-    uz: {
-        greeting: "Veb-saytimizga xush kelibsiz!",
-        description: "Bu useContext yordamida yaratilgan oddiy til almashtirgich.",
-    },
-};
-
-const Content = () => {
-    const { language } = useLanguage();
-
+const App = () => {
     return (
-        <div className="container">
-            <h1>{translations[language].greeting}</h1>
-            <p>{translations[language].description}</p>
+        <div>
+            <Quiz/>
         </div>
-    );
-};
-
-const App: React.FC = () => {
-    return (
-        <LanguageProvider>
-            <div className="app">
-                <LanguageSwitcher />
-                <Content />
-            </div>
-        </LanguageProvider>
-    );
-};
-
-export default App;
+    )
+}
+export default App
